2dc434e4933ee496bfb3c60aceaac455b80e94bc
Labais <reinitto@gmail.com>
2019-02-16 22:03:02 +0100

    addde post route for contact info

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -1,57 +1,82 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
-const Vivus = require("vivus");
-const form = document.getElementById("form-form");
-const inputs = [...document.getElementsByTagName("input")];
-const textarea = document.getElementById("your-message");
-const sandwich = document.getElementById("sandwich");
-const navItems = [...document.getElementsByClassName("navItem")];
-const introItems = [...document.querySelectorAll("#list a")];
-form.addEventListener("submit", function(e) {
-  e.preventDefault();
-  console.log("isvalid:", formValid);
-  console.log("text:", textarea.value);
-  inputs.forEach(input => {
-    console.log(input.id + " : " + input.value);
-  });
-});
+document.addEventListener("DOMContentLoaded", function(e) {
+  const Vivus = require("vivus");
+  const btnsubmit = document.getElementById("btnsubmit");
+  const form = document.getElementById("form-form");
+  const inputs = [...document.getElementsByTagName("input")];
+  const message = document.getElementsByTagName("textarea")[0];
+  // const message = document.getElementById("your-message").value;
+  // const company = document.getElementById("your-company").value;
+  // const phone = document.getElementById("your-phone").value;
+  // const email = document.getElementById("your-email").value;
+  // const name = document.getElementById("your-name").value;
+  const sandwich = document.getElementById("sandwich");
+  const navItems = [...document.getElementsByClassName("navItem")];
+  const introItems = [...document.querySelectorAll("#list a")];
+  // form.addEventListener("submit", function(e) {
+  //   console.log("text:", textarea.value);
+  //   inputs.forEach(input => {
+  //     console.log(input.id + " : " + input.value);
+  //   });
+  //   return false;
+  // });
 
-sandwich.addEventListener("click", () => {
-  navItems.forEach(item => item.classList.toggle("navItem-visible"));
-});
-
-introItems.forEach(item => {
-  item.addEventListener("mouseenter", function(e) {
-    e.stopPropagation();
-    // let children = Array.from(this.children);
-    item.children[0].classList.toggle("triangle-right-hover");
+  sandwich.addEventListener("click", () => {
+    navItems.forEach(item => item.classList.toggle("navItem-visible"));
   });
-  item.addEventListener("mouseleave", function(e) {
-    e.stopPropagation();
-    // let children = Array.from(this.children);
-    item.children[0].classList.toggle("triangle-right-hover");
-  });
-});
 
-if (window.screen.width > 996) {
-  new Vivus(
-    "reinis",
-    {
-      duration: 200
-    },
-    function() {
+  introItems.forEach(item => {
+    item.addEventListener("mouseenter", function(e) {
+      e.stopPropagation();
+      // let children = Array.from(this.children);
+      item.children[0].classList.toggle("triangle-right-hover");
+    });
+    item.addEventListener("mouseleave", function(e) {
+      e.stopPropagation();
+      // let children = Array.from(this.children);
+      item.children[0].classList.toggle("triangle-right-hover");
+    });
+  });
+
+  if (window.screen.width > 996) {
+    new Vivus(
+      "reinis",
+      {
+        duration: 200
+      },
+      function() {
+        return;
+      }
+    );
+    new Vivus("welcome-svg", { duration: 200 }, function() {
       return;
-    }
-  );
-  new Vivus("welcome-svg", { duration: 200 }, function() {
-    return;
-  });
-  new Vivus("my-portfolio-svg", { duration: 200 }, function() {
-    return;
-  });
-  new Vivus("contact-me-svg", { duration: 200 }, function() {
-    return;
-  });
-}
+    });
+    new Vivus("my-portfolio-svg", { duration: 200 }, function() {
+      return;
+    });
+    new Vivus("contact-me-svg", { duration: 200 }, function() {
+      return;
+    });
+  }
+
+  const submitForm = function() {
+    let request = {
+      message: message.value
+    };
+    inputs.forEach(input => {
+      request[input.id] = input.value;
+    });
+
+    console.log(request);
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "/contact", true);
+    var json_upload = "request=" + JSON.stringify(request);
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    //xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    xhr.send(json_upload);
+  };
+  btnsubmit.addEventListener("click", submitForm);
+});
 
 },{"vivus":2}],2:[function(require,module,exports){
 /**
--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+const bodyParser = require("body-parser");
 app.use("/public", express.static(path.resolve(__dirname + "/public")));
+app.use(bodyParser.urlencoded({ extended: false, new: true }));
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
+});
+
+app.post("/contact", (req, res) => {
+  console.log("req.body", req.body);
+  console.log("parsed:", JSON.parse(req.body.request));
+  console.log("request:", req.body.request);
+  console.log("request received");
 });
 
 const port = process.env.PORT || 5000;
